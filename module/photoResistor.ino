b39b8af981088417d1659eaf48c01ece8f92988a
Ethan <metrixlambert@gmail.com>
2019-07-31 22:32:37 +0800

    photoResistor

---
+++ module/photoResistor.ino
@@ -0,0 +1,20 @@
+int photocellPin = 2;    
+int ledPin = 13;         
+int val = 0;             
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = analogRead(photocellPin);    
+  Serial.println(val);
+
+  if(val<=112){
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWrite(ledPin, LOW);
+  }
+}
